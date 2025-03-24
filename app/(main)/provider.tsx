8d82a60f6542efa156c369dc98e52e9c481c065c
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-03-24 23:59:29 +0800

    Create main provider and context

---
+++ app/(main)/provider.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import React, { useContext, useEffect, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { GetAuthUserData } from '@/services/GlobalApi';
+
+import Header from './_components/Header';
+import { useConvex } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+
+import { AuthContext } from '@/context/AuthContext';
+import { AssistantContext } from '@/context/AssistantContext';
+
+function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const router = useRouter();
+  const convex = useConvex();
+  const { user, setUser } = useContext(AuthContext);
+  const [assistant, setAssistant] = useState();
+
+  useEffect(() => {
+    CheckUseAuth();
+  }, []);
+
+  const CheckUseAuth = async () => {
+    // Get user token from local storage
+    const token = localStorage.getItem('user_token');
+
+    // Get new access token
+    const user = token ? await GetAuthUserData(token) : null;
+
+    // If user is not logged in, redirect to sign-in page
+    if (!user?.email) {
+      router.replace('/sign-in');
+      return;
+    }
+
+    // Get User Info From Database
+    try {
+      const result = await convex.query(api.users.GetUser, {
+        email: user?.email,
+      });
+      setUser(result);
+    } catch (e) {}
+  };
+
+  return (
+    <div>
+      <AssistantContext.Provider value={{ assistant, setAssistant }}>
+        <Header />
+        {children}
+      </AssistantContext.Provider>
+    </div>
+  );
+}
+
+export default Provider;
