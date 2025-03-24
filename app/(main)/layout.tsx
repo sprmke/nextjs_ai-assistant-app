0ea098c2564c3e662b40cb0c44af37d49294eaa3
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-03-24 23:57:56 +0800

    Add new main layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Provider from './provider';
+
+function WorkspaceLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <Provider>
+                {children}
+            </Provider>
+        </div>
+    )
+}
+
+export default WorkspaceLayout
