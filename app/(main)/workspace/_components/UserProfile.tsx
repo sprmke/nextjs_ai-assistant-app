e89440815e45bd5e91f6891bf217a756f67d5a11
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-02 03:13:41 +0800

    Update user profile dialog to display real token usages

--- app/(main)/workspace/_components/UserProfile.tsx
+++ app/(main)/workspace/_components/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import {
   Dialog,
@@ -25,7 +25,13 @@ function UserProfile({
   setOpenUserProfile: (open: boolean) => void;
 }) {
   const { user } = useContext(AuthContext);
+
+  const [userMaxToken, setUserMaxToken] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setUserMaxToken(user?.orderId ? 10000 : 5000);
+  }, [user?.orderId]);
 
   return (
     <Dialog open={openUserProfile} onOpenChange={setOpenUserProfile}>
@@ -37,7 +43,7 @@ function UserProfile({
         <div className="mt-4">
           <div className="flex gap-4 items-center">
             <Image
-              src={user?.picture}
+              src={user?.picture ?? ''}
               alt="user"
               width={60}
               height={60}
@@ -54,8 +60,10 @@ function UserProfile({
           <div className="flex flex-col gap-2">
             <div className="flex flex-col gap-2">
               <p className="font-bold">Token Usage</p>
-              <p>0/0</p>
-              <Progress value={33} />
+              <p>
+                {user?.credits ?? 0}/{userMaxToken}
+              </p>
+              <Progress value={((user?.credits ?? 0) / userMaxToken) * 100} />
             </div>
 
             <div className="flex justify-between">
@@ -72,7 +80,7 @@ function UserProfile({
                 <div className="flex justify-between">
                   <div>
                     <p className="font-bold text-lg">Pro Plan</p>
-                    <p>500,000 Tokens</p>
+                    <p>10,000 Tokens</p>
                   </div>
                   <p className="font-bold text-lg flex items-center justify-center">
                     $10/month
