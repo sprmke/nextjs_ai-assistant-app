e93b6da52b9621a901b9669a563c38e472a9449c
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-02 05:44:10 +0800

    Cleanup eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  }
-}; 
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
