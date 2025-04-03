4cb6dc282c387c1dbcc505c53f0eaf8209e769ee
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-04 07:43:23 +0800

    Improve main header

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,25 +1,65 @@
 'use client';
 
-import { AuthContext } from '@/context/AuthContext';
+import React, { useContext, useState } from 'react';
+
 import Image from 'next/image';
-import React, { useContext } from 'react';
+import Link from 'next/link';
+
+import { AuthContext } from '@/context/AuthContext';
+import UserProfile from '../workspace/_components/UserProfile';
+import { LogOut } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { UserCircle2 } from 'lucide-react';
 
 function Header() {
   const { user } = useContext(AuthContext);
 
-  return (
-    <div className="p-3 fixed top-0 shadow-sm w-full flex justify-between items-center px-14">
-      <Image src={'/logo.svg'} alt="App Logo" width={40} height={40} />
+  const [openUserProfile, setOpenUserProfile] = useState(false);
 
-      {user?.picture && (
-        <Image
-          src={user?.picture}
-          alt="App Logo"
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-      )}
+  return (
+    <div className="top-0 bg-white p-3 shadow-sm w-full flex justify-between items-center px-5 fixed z-10">
+      <Link href="/">
+        <Image src={'/logo.svg'} alt="App Logo" width={40} height={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          {user?.picture && (
+            <Image
+              src={user?.picture}
+              alt="App Logo"
+              width={40}
+              height={40}
+              className="rounded-full cursor-pointer"
+              onClick={() => setOpenUserProfile(true)}
+            />
+          )}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-[200px]" align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setOpenUserProfile(true)}>
+            <UserCircle2 />
+            Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LogOut />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <UserProfile
+        openUserProfile={openUserProfile}
+        setOpenUserProfile={setOpenUserProfile}
+      />
     </div>
   );
 }
