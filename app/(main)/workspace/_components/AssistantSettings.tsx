7a272c03f3e937b90851aea261e4c3fe9bc8ea96
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-06-09 19:35:47 +0800

    Improve responsiveness of right sidebar

--- app/(main)/workspace/_components/AssistantSettings.tsx
+++ app/(main)/workspace/_components/AssistantSettings.tsx
@@ -72,67 +72,71 @@ function AssistantSettings() {
 
   return (
     assistant && (
-      <div className="p-5 bg-secondary border-l-[1px] h-screen">
-        <BlurFade delay={0.25}>
-          <div className="mt-4 flex gap-3">
-            <Image
-              src={assistant?.image}
-              alt="assistant"
-              width={100}
-              height={100}
-              className="rounded-xl h-[80px] w-[80px] object-cover"
-            />
-            <div>
-              <h2 className="font-bold">{assistant?.name}</h2>
-              <p className="text-gray-700 dark:text-gray-300">
-                {assistant?.title}
-              </p>
+      <div className="flex flex-col justify-between p-5 bg-secondary border-l-[1px] h-[calc(100vh-64px)]">
+        <div className="flex flex-col">
+          <BlurFade delay={0.25}>
+            <div className="mt-4 flex gap-3">
+              <Image
+                src={assistant?.image}
+                alt="assistant"
+                width={100}
+                height={100}
+                className="rounded-xl h-[80px] w-[80px] object-cover"
+              />
+              <div>
+                <h2 className="font-bold">{assistant?.name}</h2>
+                <p className="text-gray-700 dark:text-gray-300">
+                  {assistant?.title}
+                </p>
+              </div>
             </div>
-          </div>
-        </BlurFade>
-        <BlurFade delay={0.25 * 2}>
-          <div className="mt-4">
-            <h2 className="text-gray-500">Model:</h2>
-            <Select
-              defaultValue={assistant.aiModelId}
-              onValueChange={(value) => onHandleInputChange('aiModelId', value)}
-            >
-              <SelectTrigger className="w-full bg-white">
-                <SelectValue placeholder="Select Model" />
-              </SelectTrigger>
-              <SelectContent>
-                {aiModelOptions.map(({ id, logo, name }, index) => (
-                  <SelectItem value={id} key={index}>
-                    <div className="flex gap-2 items-center m-1">
-                      <Image
-                        src={logo}
-                        alt={name}
-                        width={20}
-                        height={20}
-                        className="rounded-md"
-                      />
-                      <h2>{name}</h2>
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-        </BlurFade>
-        <BlurFade delay={0.25 * 3}>
-          <div className="mt-4">
-            <h2 className="text-gray-500">Instructions:</h2>
-            <Textarea
-              placeholder="Add Instructions"
-              className="h-[180px] bg-white"
-              value={assistant?.userInstruction}
-              onChange={(e) =>
-                onHandleInputChange('userInstruction', e.target.value)
-              }
-            />
-          </div>
-        </BlurFade>
-        <div className="absolute bottom-10 right-5 flex gap-5">
+          </BlurFade>
+          <BlurFade delay={0.25 * 2}>
+            <div className="mt-4">
+              <h2 className="text-gray-500">Model:</h2>
+              <Select
+                defaultValue={assistant.aiModelId}
+                onValueChange={(value) =>
+                  onHandleInputChange('aiModelId', value)
+                }
+              >
+                <SelectTrigger className="w-full bg-white">
+                  <SelectValue placeholder="Select Model" />
+                </SelectTrigger>
+                <SelectContent>
+                  {aiModelOptions.map(({ id, logo, name }, index) => (
+                    <SelectItem value={id} key={index}>
+                      <div className="flex gap-2 items-center m-1">
+                        <Image
+                          src={logo}
+                          alt={name}
+                          width={20}
+                          height={20}
+                          className="rounded-md"
+                        />
+                        <h2>{name}</h2>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </BlurFade>
+          <BlurFade delay={0.25 * 3}>
+            <div className="mt-4">
+              <h2 className="text-gray-500">Instructions:</h2>
+              <Textarea
+                placeholder="Add Instructions"
+                className="h-[180px] bg-white"
+                value={assistant?.userInstruction}
+                onChange={(e) =>
+                  onHandleInputChange('userInstruction', e.target.value)
+                }
+              />
+            </div>
+          </BlurFade>
+        </div>
+        <div className="flex justify-center xl:justify-end items-center gap-3 flex-wrap">
           <AssistantConfirmationAlert OnDelete={OnDelete}>
             <Button disabled={loading} variant="ghost">
               <Trash /> Delete
