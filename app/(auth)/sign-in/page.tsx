699701839d40a65cf8d4f328a422f202c9525f70
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-06-15 16:38:45 +0800

    Improve sign in page UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -49,13 +49,10 @@ function SignIn() {
   });
 
   return (
-    <div className="flex items-center flex-col justify-center h-screen">
-      <div
-        className="flex flex-col items-center
-        gap-5 border rounded-2xl p-10 shadow-md"
-      >
+    <div className="flex items-center flex-col justify-center h-screen px-5">
+      <div className="flex flex-col items-center gap-5 border rounded-2xl p-10 shadow-md w-full md:w-[400px]">
         <Image src={'/logo.svg'} alt="App Logo" width={50} height={50} />
-        <h2 className="text-2xl">Sign In to AI Personal Companion</h2>
+        <h2 className="text-2xl">Sign In</h2>
 
         <Button onClick={() => googleLogin()}>Sign in with Google</Button>
       </div>
